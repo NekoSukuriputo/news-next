e00ef3109f4ae4c0f2767800f29a74808164cb26
abdul <abdul.rahmat@360kredi.id>
2024-07-19 14:47:51 +0700

    adding interseption page

--- app/news/[slug]/image/page.js
+++ app/news/[slug]/image/page.js
@@ -1,12 +1,17 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import { notFound } from "next/navigation";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function ImagePage({ params }) {
-  const newsSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find((item) => item.slug === newsSlug);
+  const newsItemSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find(
+    (newsItem) => newsItem.slug === newsItemSlug
+  );
+
   if (!newsItem) {
     notFound();
   }
+
   return (
     <div className="fullscreen-image">
       <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
